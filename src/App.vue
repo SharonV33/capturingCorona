97a1b2b6c0000f21ce49a4c0cdbb1dd1a8d52a5c
sharonV33 <sharonveldman33@gmail.com>
2021-01-04 16:20:25 +0100

    general styling rules static navigation

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,33 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <ul>
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li>
+      <router-link to="/about">About</router-link>
+      </li>
+      <li>
+        <a>cijfers</a>
+      </li>
+      <li>
+        <a>Arbeid en inkomen</a>
+      </li>
+      <li>
+        <a>Economie</a>
+      </li>
+      <li>
+        <a>Maatschappij</a>
+      </li>
+      <li>
+        <a>Regio</a>
+      </li>
+      <li>
+        corporate
+      </li>
+    </ul>
   </div>
+
   <router-view/>
 </template>
 
@@ -26,6 +51,25 @@ h3 {
   color: #271d6c;
   margin-bottom: 8px;
   font-size: 1.25rem;
+}
+
+#nav {
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  border-bottom: 1px solid #eee;
+}
+
+#nav ul {
+  list-style: none;
+  display: flex;
+  flex-direction: row;
+  width: 40vw;
+}
+
+#nav ul li a {
+  font-size: .9rem;
+  padding-right: 15px;
 }
 
 /*h3 titles #c90c0f*/
