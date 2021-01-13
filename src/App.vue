ab4ed68249642f16527f539026aadfe588225e9f
sharonV33 <sharonveldman33@gmail.com>
2021-01-13 10:16:41 +0100

    add text to vategory navigation

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@
       </section>
       <section id="search">
         <form>
-          <input type="text" placeholder="stel je vraag">
+          <input type="text" placeholder="Stel je vraag">
         </form>
       </section>
     </section>
@@ -39,36 +39,41 @@
         <div class="economy">
           <router-link to="/">
           <img src="@/assets/economy.svg">
+            <p>Economie</p>
           </router-link>
         </div>
         <div class="social">
           <img src="@/assets/social.svg">
+          <p>Sociaal</p>
         </div>
         <div class="medical">
           <img src="@/assets/medical.svg">
+          <p>Medisch</p>
         </div>
         <div class="product">
           <img src="@/assets/product.svg">
+          <p>product & handel</p>
         </div>
         <div class="government">
           <img src="@/assets/government.svg">
+          <p>Overheid</p>
         </div>
         <div class="transport">
           <router-link to="/transport">
             <img src="@/assets/transport.svg">
+            <p>Transport & vervoer</p>
           </router-link>
         </div>
         <div class="work">
           <img src="@/assets/work.svg">
+          <p>Werk & look</p>
         </div>
       </section>
       <section class="bodyContent">
         <router-view/>
       </section>
     </section>
-
   </div>
-
 </template>
 
 <style lang="scss">
