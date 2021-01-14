78d224e43b20296aa340a44e1267f5fa971d4374
sharonV33 <sharonveldman33@gmail.com>
2021-01-14 10:54:22 +0100

    work on routers

--- src/views/transport.vue
+++ src/views/transport.vue
@@ -2,16 +2,22 @@
   <div class="contentOverview">
     <section class="smallArticle">
       <SmallArticle
-        question="oe ontwikkelt het aantal passagiers dat van en naar nationale luchthavens vliegt zich"
+        question="Hoe ontwikkelt het aantal passagiers dat van en naar nationale luchthavens vliegt zich"
         icon="fas fa-plane"
         :percentage="12"
       />
     </section>
     <section class="smallArticle">
-      <SmallArticle question="Hoeveel minder reizen we met het openbaar vervoer" icon="fas fa-train" :percentage="-99"/>
+      <SmallArticle
+              question="Hoeveel minder reizen we met het openbaar vervoer"
+              icon="fas fa-train"
+              :percentage="-99"/>
     </section>
     <section class="smallArticle">
-      <SmallArticle question="Hoe ontwikkelt het goederenvervoer zich" icon="fas fa-truck" :percentage="99"/>
+      <SmallArticle
+              question="Hoe ontwikkelt het goederenvervoer zich"
+              icon="fas fa-truck"
+              :percentage="99"/>
     </section>
     <section class="smallArticle">
       <NegativeSmallArticle />
