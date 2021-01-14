f078945b62a8ee82a1488d16ade7bb5b65df8761
sharonV33 <sharonveldman33@gmail.com>
2021-01-14 10:54:00 +0100

    fix code inconsistency

--- src/views/work.vue
+++ src/views/work.vue
@@ -3,16 +3,22 @@
         <h1>WERK</h1>
         <section class="smallArticle">
             <SmallArticle
-                    question="oe ontwikkelt het aantal passagiers dat van en naar nationale luchthavens vliegt zich"
+                    question="Hoe ontwikkelt het aantal passagiers dat van en naar nationale luchthavens vliegt zich"
                     icon="fas fa-plane"
                     :percentage="12"
             />
         </section>
         <section class="smallArticle">
-            <SmallArticle question="Hoeveel minder reizen we met het openbaar vervoer" icon="fas fa-train" :percentage="-99"/>
+            <SmallArticle
+                    question="Hoeveel minder reizen we met het openbaar vervoer"
+                    icon="fas fa-train"
+                    :percentage="-99"/>
         </section>
         <section class="smallArticle">
-            <SmallArticle question="Hoe ontwikkelt het goederenvervoer zich" icon="fas fa-truck" :percentage="99"/>
+            <SmallArticle
+                    question="Hoe ontwikkelt het goederenvervoer zich"
+                    icon="fas fa-truck"
+                    :percentage="99"/>
         </section>
         <section class="smallArticle">
             <NegativeSmallArticle />
@@ -52,6 +58,8 @@
         width:80%;
         padding-top: 1rem;
     }
+
+
 
     .largeArticle {
         border: 1px solid darkgray;
